019e7aa5aa9adad154bd4689bdd4aa0819deb299
kvjsjdah1 <kvjsjdah1@gmail.com>
2026-08-25 13:33:51 +0800

    index.js

--- bridge/index.js
+++ bridge/index.js
@@ -3,6 +3,13 @@ import crypto from 'crypto';
 
 const app = express();
 app.use(express.json());
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Secret');
+  if (req.method === 'OPTIONS') return res.sendStatus(204);
+  next();
+});
 
 const SECRET = process.env.BRIDGE_SECRET || 'changeme';
 const PORT = process.env.PORT || 3000;
